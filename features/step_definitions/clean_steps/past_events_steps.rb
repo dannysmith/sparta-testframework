efb671297e2a8e6a90c0c18dab34653379472cd1
Jane Last <jane.kempton@gmail.com>
2014-11-03 16:26:29 +0000

    created past events steps file

---
+++ features/step_definitions/clean_steps/past_events_steps.rb
@@ -0,0 +1,11 @@
+Given(/^there is a past event$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I go to the calendar page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a past events$/) do
+  pending # express the regexp above with the code you wish you had
+end
