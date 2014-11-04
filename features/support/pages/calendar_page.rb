9720956134e6fe29e920689454b4104b9f8be65d
Aaron <aaron.muir21@gmail.com>
2014-11-04 19:02:24 +0000

    calendar page added

--- features/support/pages/calendar_page.rb
+++ features/support/pages/calendar_page.rb
@@ -3,5 +3,13 @@ class CalendarPage < HomePage
 
   def visit_calendar
     @browser.element(:title, "Spartiaite LMS").click
+    @browser.a(:href, "http://unix.spartaglobal.com/moodle3/calendar/view.php?view=month")
   end 
+
+  def create_event
+    @browser.element(:text, "New Event")
+    @browser.text_field(:id, "id_name").set "Test Event"
+    @browser.text_field(:id, "id_descriptionedittable").set "Test event description"
+    
+  end
 end
