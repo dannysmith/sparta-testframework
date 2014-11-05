78c22dda96ebe068fe9ffa809cca9c18143d6b86
Aaron Muir <aaron.muir21@gmail.com>
2014-11-05 11:24:44 +0000

    Calendar page object updated to create events" "

--- features/support/pages/calendar_page.rb
+++ features/support/pages/calendar_page.rb
@@ -1,15 +1,15 @@
 require_relative 'home_page'
 class CalendarPage < HomePage
 
-  def visit_calendar
-    @browser.element(:title, "Spartiaite LMS").click
-    @browser.a(:href, "http://unix.spartaglobal.com/moodle3/calendar/view.php?view=month")
+  def visit
+    @browser.goto "http://unix.spartaglobal.com/moodle3/calendar/view.php?view=month"
   end 
 
   def create_event
-    @browser.element(:text, "New Event")
+    @browser.goto "http://unix.spartaglobal.com/moodle3/calendar/event.php?action=new&course"
     @browser.text_field(:id, "id_name").set "Test Event"
-    @browser.text_field(:id, "id_descriptionedittable").set "Test event description"
-    
+    @browser.select_list(:id, "id_timestart_day").select_value(10)
+    @browser.select_list(:id, "id_timestart_month").select_value(11)
+    @browser.button(:id, "id_submitbutton").click
   end
 end
