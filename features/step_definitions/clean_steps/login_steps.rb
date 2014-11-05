cb18e6a4078ee95d9b07e37900472c0a487f4054
Trishul <trishulgohil@live.co.uk>
2014-11-05 16:47:31 +0000

    MDL-1 Guest login sceario written, merged incorrect username and password into one scenario and written.

--- features/step_definitions/clean_steps/login_steps.rb
+++ features/step_definitions/clean_steps/login_steps.rb
@@ -1,94 +1,90 @@
 
 Given(/^that I am on the Moodle Login Page to login as admin$/) do
-  @app.home.visit
-  @app.home.visit_login
+  @app.login.visit
 
   expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php") 
 end
 
 When(/^I log into Moodle as an Admin$/) do
-  @app.login.login_admin
+  @app.login.admin
 
   expect(@browser.a(:title, "Admin User").text).to eq("Admin User") 
 end
 
 Then(/^I should be on the Admin Dashboard page$/) do
   expect(@browser.p(:id, "expandable_branch_71_siteadministration").text).to eq("Site administration")
-  
-  @app.home.logout
+  @app.login.logout
 end
 
 
 
 
-Given(/^that I am on the login page to login as a student$/) do
-  @app.home.visit
-  @app.home.visit_login
+Given(/^that I am on the login page to login as a user$/) do
+  @app.login.visit
 
   expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php") 
 end
 
-When(/^I log into Moodle as an student$/) do
-  @app.login.login_user
+When(/^I log into Moodle as an user$/) do
+  @app.login.user
   
   expect(@browser.a(:title, "Aaron Muir").text).to eq("Aaron Muir")
 end
 
-Then(/^I should be on the Student Dashboard page$/) do
+Then(/^I should be on the user Dashboard page$/) do
   expect(@browser.text.include?("Site Admin")).to be false
   
-  @app.home.logout
+  @app.login.logout
 end
 
 
 
 
 Given(/^I am on the login page to login as a guest$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.login.visit
+
+  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
 end
 
-When(/^I select the "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
+When(/^I select the Log in as a guest$/) do
+  @app.login.guest
+  
+  expect(@browser.a(:title, "Guest user  ").text).to eq("Guest user") 
 end
 
 Then(/^I should be on the Guest Dashboard page$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@browser.text.include?("Site Admin")).to be false
+  
+  @app.login.logout
 end
 
 
 
 
 Given(/^I am on the login page to login with an incorrect password$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.login.visit
+
+  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
 end
 
 When(/^I attempt to login with an incorrect password$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^an error should be shown$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.login.wrong_password
+  
+  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
 end
 
 Then(/^I am denied access and returned to the login page$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
 end
 
-
-
-
 Given(/^I am on the login page to login with an incorrect username$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.login.visit
 end
 
 When(/^I attempt to login with an incorrect username$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Then(/^an error message should be shown$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.login.wrong_username
 end
 
 Then(/^returned to the login page and not logged in$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
 end
