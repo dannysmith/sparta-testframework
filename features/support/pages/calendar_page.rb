a2b93021d45eeb4457ae99087a4efc9378e5fe61
Jane Last <jane.kempton@gmail.com>
2014-11-05 16:38:47 +0000

    MDL-19 Changed name of populate_events to past_events_list and moved to Private

--- features/support/pages/calendar_page.rb
+++ features/support/pages/calendar_page.rb
@@ -1,37 +1,22 @@
 require_relative 'home_page'
+
 require 'date'
 require 'rspec'
 class CalendarPage < HomePage
 
-  def visit_calendar
+  def visit
      @browser.goto "http://unix.spartaglobal.com/moodle3/calendar/view.php?view=month"
   end 
   
-  def populate_events
-    today = Date.today.day
-    calendar = @browser.table(:class, "calendarmonth calendartable")
-    rows = calendar.trs
-    @past_events = []
-    rows.each_with_index do |row,i|
-      next if i == 0
-      
-      cell = row.tds
-      
-      cell.each do |data|
-        @past_events.push(data.ul.li.a.text) if data.text.to_i < today && data.text.to_i != 0 && data.ul.exists?
-      end
-    end
-  end
-  
   def check_events
             
-    populate_events
+    past_events_list
 
     expect(@past_events.empty?).to be(false)
   end
   
   def choose_past_events
-    populate_events
+    past_events_list
     
     @browser.element(:text, @past_events.last).click
     expect(@browser.text).to include "Day view"
@@ -43,4 +28,21 @@ class CalendarPage < HomePage
     @browser.text_field(:id, "id_descriptionedittable").set "Test event description"
     
   end
+  
+private
+  def past_events_list
+    today = Date.today.day
+    calendar = @browser.table(:class, "calendarmonth calendartable")
+    rows = calendar.trs
+    @past_events = []
+    rows.each_with_index do |row,i|
+      next if i == 0
+       
+        cell = row.tds
+        
+        cell.each do |data|
+          @past_events.push(data.ul.li.a.text) if data.text.to_i < today && data.text.to_i != 0 && data.ul.exists?
+        end
+      end
+   end
 end
