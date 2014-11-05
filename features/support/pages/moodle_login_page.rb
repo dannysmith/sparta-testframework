ade2b3647bbc0e43f9e8a863a986be2bd8bb6a36
Trishul <trishulgohil@live.co.uk>
2014-11-05 11:37:02 +0000

    removed login_ from method names for easier reading

--- features/support/pages/moodle_login_page.rb
+++ features/support/pages/moodle_login_page.rb
@@ -3,19 +3,19 @@ require_relative 'generic'
 class MoodleLogin < GenericPage
   
   
-  def login_user
-    @browser.text_field(:name, "username").set(CREDENTIALS[1][:user])
-    @browser.text_field(:name, "password").set(CREDENTIALS[1][:password])
+  def user
+    @browser.text_field(:name, "username").set(CREDENTIALS[:normal][0][:user])
+    @browser.text_field(:name, "password").set(CREDENTIALS[:normal][0][:password])
     @browser.button(:class, "fa-angle-right").click
   end
   
-  def login_admin
+  def admin
     @browser.text_field(:name, "username").set(CREDENTIALS[:admin][0][:user])
     @browser.text_field(:name, "password").set(CREDENTIALS[:admin][0][:password])
     @browser.button(:class, "fa-angle-right").click
   end
   
-  def login_guest
+  def guest
     @browser.button(:value, "Log in as a guest").click
   end  
 end
