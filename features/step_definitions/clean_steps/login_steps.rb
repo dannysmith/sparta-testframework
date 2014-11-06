ff75e8f26d3fb9bd36c50433ce431a59a6a99f12
Trishul <trishulgohil@live.co.uk>
2014-11-06 10:22:20 +0000

    MDL-1 Used bashboard page-object methods instead of accessing UI through step definitions

--- features/step_definitions/clean_steps/login_steps.rb
+++ features/step_definitions/clean_steps/login_steps.rb
@@ -2,17 +2,17 @@
 Given(/^that I am on the Moodle Login Page to login as admin$/) do
   @app.login.visit
 
-  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php") 
+  expect(@browser.url).to eq("#{EnvConfig.base_url}login/index.php") 
 end
 
 When(/^I log into Moodle as an Admin$/) do
   @app.login.admin
 
-  expect(@browser.a(:title, "Admin User").text).to eq("Admin User") 
+  expect(@app.dashboard.admin).to eq("Admin User") 
 end
 
 Then(/^I should be on the Admin Dashboard page$/) do
-  expect(@browser.p(:id, "expandable_branch_71_siteadministration").text).to eq("Site administration")
+  expect(@app.dashboard.admin_panel).to eq("Site administration")
   @app.login.logout
 end
 
@@ -22,13 +22,13 @@ end
 Given(/^that I am on the login page to login as a user$/) do
   @app.login.visit
 
-  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php") 
+  expect(@browser.url).to eq("#{EnvConfig.base_url}login/index.php") 
 end
 
 When(/^I log into Moodle as an user$/) do
   @app.login.user
   
-  expect(@browser.a(:title, "Aaron Muir").text).to eq("Aaron Muir")
+  expect(@app.dashboard.user).to eq("Aaron Muir")
 end
 
 Then(/^I should be on the user Dashboard page$/) do
@@ -43,17 +43,17 @@ end
 Given(/^I am on the login page to login as a guest$/) do
   @app.login.visit
 
-  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
+  expect(@browser.url).to eq("#{EnvConfig.base_url}login/index.php")
 end
 
-When(/^I select the Log in as a guest$/) do
+When(/^I choose to login as a guest$/) do
   @app.login.guest
   
-  expect(@browser.a(:title, "Guest user  ").text).to eq("Guest user") 
+  expect(@app.dashboard.guest).to eq("Guest user") 
 end
 
 Then(/^I should be on the Guest Dashboard page$/) do
-  expect(@browser.text.include?("Site Admin")).to be false
+  expect(@app.dashboard.site_admin).to be false
   
   @app.login.logout
 end
@@ -64,17 +64,17 @@ end
 Given(/^I am on the login page to login with an incorrect password$/) do
   @app.login.visit
 
-  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
+  expect(@browser.url).to eq("#{EnvConfig.base_url}login/index.php")
 end
 
 When(/^I attempt to login with an incorrect password$/) do
   @app.login.wrong_password
   
-  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
+  expect(@browser.url).to eq("#{EnvConfig.base_url}login/index.php")
 end
 
 Then(/^I am denied access and returned to the login page$/) do
-  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
+  expect(@browser.url).to eq("#{EnvConfig.base_url}login/index.php")
 end
 
 Given(/^I am on the login page to login with an incorrect username$/) do
@@ -86,5 +86,5 @@ When(/^I attempt to login with an incorrect username$/) do
 end
 
 Then(/^returned to the login page and not logged in$/) do
-  expect(@browser.url).to eq("http://unix.spartaglobal.com/moodle3/login/index.php")
+  expect(@browser.url).to eq("#{EnvConfig.base_url}login/index.php")
 end
