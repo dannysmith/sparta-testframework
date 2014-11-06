b90028ba0faaeabe2236f084faa67c2a70a30945
Trishul <trishulgohil@live.co.uk>
2014-11-06 10:17:36 +0000

    MDL-1 created dashboard Page-Object for searching elements on the dashboard after logging in

---
+++ features/support/pages/dashboard_page.rb
@@ -0,0 +1,23 @@
+require_relative 'generic'
+
+class Dashboard < GenericPage
+  def site_admin
+    @browser.text.include?("Site Admin")
+  end
+  
+  def admin_panel
+    @browser.p(:id, "expandable_branch_71_siteadministration").text
+  end
+  
+  def guest
+    @browser.a(:title, "Guest user  ").text
+  end
+  
+  def user
+    @browser.a(:title, "Aaron Muir").text
+  end
+  
+  def admin
+    @browser.a(:title, "Admin User").text
+  end
+end
