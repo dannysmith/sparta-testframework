3c9d25520e0f6143548d07e75f08a26747128d0c
romeoledesma <cyphercount@yahoo.co.uk>
2014-11-06 10:34:56 +0000

    Changed method login to login_as. This method is temporary as I will be using Trishul's login Page Object soon

--- features/support/pages/moodle_login_page.rb
+++ features/support/pages/moodle_login_page.rb
@@ -2,16 +2,16 @@ require_relative 'generic'
 
 class MoodleLoginPage < GenericPage
   
-  ADMIN_LOGIN = {username: "moodle", password: "soXy3zX2JTRwZCXw!"}
+  ADMIN_LOGIN = {username: 'moodle', password: 'soXy3zX2JTRwZCXw!'}
   STUDENT_LOGIN = {username: "user", password: "My.password1"}
   
-  def login user_type
+  def login_as user_type
     @browser.a(text: 'Log in').click
     
-    if user_type == 'admin'
+    if user_type == :admin
       @browser.text_field(name: 'username').set(ADMIN_LOGIN[:username])
       @browser.text_field(name: 'password').set(ADMIN_LOGIN[:password])
-    elsif user_type == 'student'
+    elsif user_type == :student
       @browser.text_field(name: 'username').set(STUDENT_LOGIN[:username])
       @browser.text_field(name: 'password').set(STUDENT_LOGIN[:password])
     else
@@ -19,5 +19,7 @@ class MoodleLoginPage < GenericPage
     end
     
     @browser.button(class: 'icon-submit fa fa-angle-right').click
+    
+    @browser.span(text: 'Site administration').wait_until_present
   end
 end
