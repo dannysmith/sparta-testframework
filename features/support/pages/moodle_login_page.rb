062b504a0324ac6e8a6bef1f7ea9acb6d3d240f8
romeoledesma <cyphercount@yahoo.co.uk>
2014-11-07 10:45:06 +0000

    MDL-1 Replaced my login method with Trishul's

--- features/support/pages/moodle_login_page.rb
+++ features/support/pages/moodle_login_page.rb
@@ -1,47 +1,36 @@
 # NEEDS TO BE REFACTORED
 class MoodleLoginPage < GenericPage
   
-  ADMIN_LOGIN = {username: 'moodle', password: 'soXy3zX2JTRwZCXw!'}
-  STUDENT_LOGIN = {username: "user", password: "My.password1"}
+  def visit
+    @browser.goto "http://unix.spartaglobal.com/moodle3/login/index.php"
+  end
   
-  def login_as user_type
-    @browser.a(text: 'Log in').click
+  def login_as_user
+    visit
     
-    if user_type == :admin
-      @browser.text_field(name: 'username').set(ADMIN_LOGIN[:username])
-      @browser.text_field(name: 'password').set(ADMIN_LOGIN[:password])
-    elsif user_type == :student
-      @browser.text_field(name: 'username').set(STUDENT_LOGIN[:username])
-      @browser.text_field(name: 'password').set(STUDENT_LOGIN[:password])
-    else
-      raise "User type can only be 'admin' or 'student'."
-    end
-    
-    @browser.button(class: 'icon-submit fa fa-angle-right').click
+    @browser.text_field(:name, "username").set(CREDENTIALS[:normal][0][:user])
+    @browser.text_field(:name, "password").set(CREDENTIALS[:normal][0][:password])
+    @browser.button(:class, "fa-angle-right").click
     
     @browser.h2(class: 'marketingheader').wait_until_present
   end
   
-  def visit
-    @browser.goto "http://unix.spartaglobal.com/moodle3/login/index.php"
-  end
-
-
-  
-  def user
-    @browser.text_field(:name, "username").set(CREDENTIALS[:normal][0][:user])
-    @browser.text_field(:name, "password").set(CREDENTIALS[:normal][0][:password])
-    @browser.button(:class, "fa-angle-right").click
-  end
-  
-  def admin
+  def login_as_admin
+    visit
+    
     @browser.text_field(:name, "username").set(CREDENTIALS[:admin][0][:user])
     @browser.text_field(:name, "password").set(CREDENTIALS[:admin][0][:password])
     @browser.button(:class, "fa-angle-right").click
+    
+    @browser.h2(class: 'marketingheader').wait_until_present
   end
   
-  def guest
+  def login_as_guest
+    visit
+    
     @browser.button(:value, "Log in as a guest").click
+    
+    @browser.h2(class: 'marketingheader').wait_until_present
   end  
   
   def wrong_password
