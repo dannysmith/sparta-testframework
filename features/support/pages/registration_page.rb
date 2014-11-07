0fb02cb41158a43243ef730a0df758b534240319
Aaron Muir <aaron.muir21@gmail.com>
2014-11-07 15:06:43 +0000

    MDL-21 register feature complete

--- features/support/pages/registration_page.rb
+++ features/support/pages/registration_page.rb
@@ -1,3 +1,31 @@
-class RegistrationPage > GenericPage
-  
+class RegistrationPage < GenericPage
+
+  EMAIL = "testingcircle.test@gmail.com"
+  PASSWORD = "Derek.password1"
+
+  def visit
+    @browser.goto "http://unix.spartaglobal.com/moodle3/login/signup.php"
+  end
+
+  def complete_registration
+    @browser.text_field(:id, "id_username").set "derek"
+    @browser.text_field(:id, "id_password").set PASSWORD
+    @browser.text_field(:id, "id_email").set EMAIL
+    @browser.text_field(:id, "id_email2").set EMAIL
+    @browser.text_field(:id, "id_firstname").set "Derek"
+    @browser.text_field(:id, "id_lastname").set "Derekson"
+
+    @browser.button(:id, "id_submitbutton").click  
+  end
+
+  def check_registration
+    @browser.goto "mail.google.com"
+    @browser.text_field(:id, "Email").set EMAIL
+    @browser.text_field(:id, "Passwd").set PASSWORD
+    @browser.button(:id, "signIn").click
+  end
+
+  def email_load_time
+    Watir::Wait.until{@browser.url == "https://mail.google.com/mail/\#inbox"}
+  end
 end
