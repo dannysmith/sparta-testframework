b52102719ee214b0ce50c41338c3099c8c095aad
romeoledesma <cyphercount@yahoo.co.uk>
2014-11-07 13:25:25 +0000

    MDL-1 Abstracted the guest login method

--- features/support/pages/moodle_login_page.rb
+++ features/support/pages/moodle_login_page.rb
@@ -5,40 +5,32 @@ class MoodleLoginPage < GenericPage
     @browser.goto "http://unix.spartaglobal.com/moodle3/login/index.php"
   end
   
-  def login_as_user
+  def login_as user_type
     visit
     
-    @browser.text_field(:name, "username").set(CREDENTIALS[:normal][0][:user])
-    @browser.text_field(:name, "password").set(CREDENTIALS[:normal][0][:password])
-    @browser.button(:class, "fa-angle-right").click
+    if user_type == :admin
+      @browser.text_field(name: 'username').set(CREDENTIALS[:admin][0][:user])
+      @browser.text_field(name: 'password').set(CREDENTIALS[:admin][0][:password])
+    elsif user_type == :student
+      @browser.text_field(name: 'username').set(CREDENTIALS[:normal][0][:user])
+      @browser.text_field(name: 'password').set(CREDENTIALS[:normal][0][:password])
+    elsif user_type == :guest
+      @browser.button(:value, "Log in as a guest").click
+      @browser.h2(class: 'marketingheader').wait_until_present
+    else
+      raise "User type can only be 'admin' or 'student'."
+    end
+    
+    @browser.button(class: 'icon-submit fa fa-angle-right').click
     
     @browser.h2(class: 'marketingheader').wait_until_present
   end
-  
-  def login_as_admin
-    visit
-    
-    @browser.text_field(:name, "username").set(CREDENTIALS[:admin][0][:user])
-    @browser.text_field(:name, "password").set(CREDENTIALS[:admin][0][:password])
-    @browser.button(:class, "fa-angle-right").click
-    
-    @browser.h2(class: 'marketingheader').wait_until_present
-  end
-  
-  def login_as_guest
-    visit
-    
-    @browser.button(:value, "Log in as a guest").click
-    
-    @browser.h2(class: 'marketingheader').wait_until_present
-  end  
   
   def wrong_password
     @browser.text_field(:name, "username").set(CREDENTIALS[:incorrect_password][0][:user])
     @browser.text_field(:name, "password").set(CREDENTIALS[:incorrect_password][0][:password])
     @browser.button(:class, "fa-angle-right").click
   end
-  
   
   def wrong_username
     @browser.text_field(:name, "username").set(CREDENTIALS[:incorrect_username][0][:user])
