7b7dec3f4a45b3602280c2b372d421205d2789a7
romeoledesma <cyphercount@yahoo.co.uk>
2014-11-07 10:50:40 +0000

    MDL-2 Created a new method that clicks on course options such as delete and edit

--- features/support/pages/moodle_course_mgmt_page.rb
+++ features/support/pages/moodle_course_mgmt_page.rb
@@ -21,4 +21,31 @@ class MoodleCourseManagementPage < GenericPage
   def course_management_page
     @browser.text.include?("Course and category management")
   end
+  
+  def click_on option
+    options = {
+               view:           0,
+               edit:           1,
+               enrolled_users: 2,
+               delete:         3,
+               hide:           4,
+               backup:         5,
+               restore:        6
+              }
+    
+    #@browser.a(text: 'Edit').when_present.click - this doesn't work so I was forced to do the following..
+  
+    @browser.div(class: 'listing-actions course-detail-listing-actions').wait_until_present
+    # course_options_list = @browser.div(class: 'listing-actions course-detail-listing-actions').wait_until_present
+    # doesn't work either
+
+    # so what i did was took the div wrapper for all the available options for a course..
+    course_options_list = @browser.div(class: 'listing-actions course-detail-listing-actions')
+    # took all the available options for a course..
+    course_options = course_options_list.as
+    # identified that option..
+    course_option = course_options[options[option]]
+    # then clicked it
+    course_option.click
+  end
 end
