2c11a11f9ec92952014601fbe9463b39c9826ca0
romeoledesma <cyphercount@yahoo.co.uk>
2014-11-10 11:38:24 +0000

    Tried a new fix for the CodeShip DOM error.

--- features/step_definitions/clean_steps/assign_course_steps.rb
+++ features/step_definitions/clean_steps/assign_course_steps.rb
@@ -34,6 +34,15 @@ And /^I successfully assign a user to be the Teacher for this newly created cour
   
   @app.course_mgmt.goto_course POST_TITLE[:course_to_be_assigned][0][:full_title]
   
+  expect(@browser.div(class: /\w*fullname/).when_present.text).to include(POST_TITLE[:course_to_be_assigned][0][:full_title])
+      expect(@browser.div(class: /\w*shortname/).when_present.text).to include(POST_TITLE[:course_to_be_assigned][0][:short_title])
+end
+
+When /^I successfully assign a user to be the Teacher for this newly created course$/ do
+  @app.course_mgmt.visit
+  
+  @app.course_mgmt.expand_software_testing_category
+  
   @app.course_mgmt.click_on :enrolled_users
   
   @app.enrolled_users.enrol 'Aaron Muir'
