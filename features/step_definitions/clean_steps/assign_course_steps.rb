e2f30349dfd7f5a7d9ae9055b2191825c460107f
romeoledesma <cyphercount@yahoo.co.uk>
2014-11-10 11:51:01 +0000

    MDL-47 Updated the step definitions to match the new assign course feature file

--- features/step_definitions/clean_steps/assign_course_steps.rb
+++ features/step_definitions/clean_steps/assign_course_steps.rb
@@ -7,19 +7,8 @@ And /^I create a new course within this category$/ do
   
   @app.course_creation.create_course
 end
-  
-Then /^this course should be successfully created$/ do
-  @app.course_mgmt.visit
-  
-  @app.course_mgmt.expand_software_testing_category
-  
-  @app.course_mgmt.goto_course POST_TITLE[:course_to_be_assigned][0][:full_title]
-  
-  expect(@browser.div(class: /\w*fullname/).when_present.text).to include(POST_TITLE[:course_to_be_assigned][0][:full_title])
-      expect(@browser.div(class: /\w*shortname/).when_present.text).to include(POST_TITLE[:course_to_be_assigned][0][:short_title])
-end
 
-When /^I successfully assign a user to be the Teacher for this newly created course$/ do
+And /^I successfully assign a user to be the Teacher for this newly created course$/ do
   @app.course_mgmt.visit
   
   @app.course_mgmt.expand_software_testing_category
