16c7fa3128fb62fd15188a784ddc84d0485bedc7
dkentuk <dk654@hotmail.co.uk>
2014-11-10 11:29:14 +0000

    @MDL-5 Added Step definitions generated from list_course.feature

---
+++ features/step_definitions/clean_steps/list_course_steps.rb
@@ -0,0 +1,23 @@
+Given(/^that I am logged in as a User$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I am on MyHome page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select all Courses$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see a list of all available courses$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I select an available course$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be taken to that course page$/) do
+  pending # express the regexp above with the code you wish you had
+end
