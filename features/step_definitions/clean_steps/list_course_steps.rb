55854618c6f710d869913dc1086fceb814487fce
dkentuk <dk654@hotmail.co.uk>
2014-11-10 14:57:37 +0000

    @MDL-5 Added code to expect the steps to be correct

--- features/step_definitions/clean_steps/list_course_steps.rb
+++ features/step_definitions/clean_steps/list_course_steps.rb
@@ -1,23 +1,22 @@
 Given(/^that I am logged in as a User$/) do
-  pending # express the regexp above with the code you wish you had
+  @app.login_page.login_as :student
+  
+  expect(@app.dashboard.user).to eq("Aaron Muir")
 end
 
-When(/^I am on MyHome page$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-When(/^I select all Courses$/) do
-  pending # express the regexp above with the code you wish you had
+When(/^I'm on the all course page$/) do
+  @app.all_course_page.visit
 end
 
 Then(/^I should see a list of all available courses$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@browser.text.include?("Miscellaneous")).to be true
+  # course_category_tree clearfix category-browse category-browse-0
 end
 
-When(/^I select an available course$/) do
-  pending # express the regexp above with the code you wish you had
+Then(/^I select an available course$/) do
+  @browser.goto "http://unix.spartaglobal.com/moodle3/course/view.php?id=52"
 end
 
 Then(/^I should be taken to that course page$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(@browser.url.should == "http://unix.spartaglobal.com/moodle3/course/view.php?id=52")
 end
